274e8e7a3987e45a65382384cdcb6d73342f6f01
kis-eleonora <79101681+kis-eleonora@users.noreply.github.com>
2022-02-13 17:46:25 +0100

    Create ellenorzes.php

--- ellenorzes.php
+++ ellenorzes.php
@@ -1,20 +1,15 @@
 <?php
 
 session_start();
-$conn = new mysqli("localhost", "root", "", "vizsgaremek");
 
-if ($conn->errno > 0) {
-    die("Adatbázis nem elérhető!");
-}
+require_once 'connect.php';
 
-$conn->set_charset("utf-8");
-
-if (!isset($_POST['email'], $_POST['jelszo'])) {
+if (empty($_POST['email'] && $_POST['jelszo'])) {
 
     exit('A mezők kitöltése kötelező!');
 }
 
-if ($stmt = $conn->prepare('SELECT `email`, `jelszo` FROM `szemelyek` WHERE `email`= ?')) {
+if ($stmt = $conn->prepare('SELECT `email`, `jelszo`, `jog_ID` FROM `szemelyek` WHERE `email`= ?')) {
 
     $stmt->bind_param('s', $_POST['email']);
     $stmt->execute();
@@ -22,22 +17,31 @@ if ($stmt = $conn->prepare('SELECT `email`, `jelszo` FROM `szemelyek` WHERE `ema
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($email, $jelszo);
+        $stmt->bind_result($email, $jelszo, $jog_ID);
         $stmt->fetch();
 
-        if (md5($_POST['jelszo']) === $jelszo) {
+        if (md5($_POST['jelszo']) === $jelszo ) {
 
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
+            if ($jog_ID === 1) {
+                header("location: vezeto.php");
+            } else {
+                 header("location: alkalmazott.php");
+            }
 
-            header("location: alkalmazott.php");
+            
         } else {
+            
             $_SESSION['error'] = 'Helytelen felhasználónév vagy jelszó!';
             header("location: index.php");
+//            echo 'Hibás jelszó';
         }
     } else {
+        
         $_SESSION['error'] = 'Helytelen felhasználónév vagy jelszó!';
         header("location: index.php");
+//        echo 'Hibás email'; 
     }
 
     $stmt->close();
